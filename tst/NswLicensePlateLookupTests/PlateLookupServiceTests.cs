b511acf709ae22542b88337a1aa81af15a3914c2
unknown <nickmurray91@gmail.com>
2019-08-27 21:28:22 +1000

    created new unit test proj and restructed file system

---
+++ tst/NswLicensePlateLookupTests/PlateLookupServiceTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace NswLicensePlateLookupTests
+{
+    public class PlateLookupServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var plateLookupService = new PlateLookupService();
+            
+            // Act 
+
+
+            // Assert
+        }
+    }
+}
