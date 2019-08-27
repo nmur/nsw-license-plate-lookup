3500f3218f4458784488ead225ad230978b98660
unknown <nickmurray91@gmail.com>
2019-08-27 21:46:24 +1000

    wrote failing test for basic empty input case

---
+++ src/NswLicensePlateLookup/Services/PlateLookupService.cs
@@ -0,0 +1,10 @@
+namespace NswLicensePlateLookup.Services
+{
+    public class PlateLookupService
+    {
+        public void GetPlateDetails(string PlateNumber)
+        {
+
+        }
+    }
+}
--- tst/NswLicensePlateLookupTests/PlateLookupServiceTests.cs
+++ tst/NswLicensePlateLookupTests/PlateLookupServiceTests.cs
@@ -1,20 +1,22 @@
 using System;
 using Xunit;
+using NswLicensePlateLookup.Services;
 
 namespace NswLicensePlateLookupTests
 {
     public class PlateLookupServiceTests
     {
         [Fact]
-        public void Test1()
+        public void GivenEmptyStringPlateNumber_WhenPlateDetailsAreRequested_ThenErrorIsReturned()
         {
             // Arrange
             var plateLookupService = new PlateLookupService();
             
             // Act 
-
+            Action action = () => plateLookupService.GetPlateDetails("");
 
             // Assert
+            Assert.Throws<ArgumentException>(action);
         }
     }
 }
