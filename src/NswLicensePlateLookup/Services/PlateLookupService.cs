3d955395c96a69309117da56511c8bde48cffe93
Nicholas Murray <nickmurray91@gmail.com>
2019-08-28 23:24:03 +1000

    added barebones call to the service API

--- src/NswLicensePlateLookup/Services/PlateLookupService.cs
+++ src/NswLicensePlateLookup/Services/PlateLookupService.cs
@@ -1,13 +1,32 @@
 using System;
+using System.Threading.Tasks;
+using Refit;
 
 namespace NswLicensePlateLookup.Services
 {
     public class PlateLookupService
     {
-        public void GetPlateDetails(string plateNumber)
+        public string GetPlateDetails(string plateNumber)
         {
             if (plateNumber == string.Empty)
                 throw new ArgumentException();
+
+            GetRequestToken();
+
+            return "";
         }
+
+        private string GetRequestToken()
+        {
+            var serviceApi = RestService.For<IPlateLookupServiceApi>("https://my.service.nsw.gov.au");
+
+            return "";
+        }
+    }
+
+    public interface IPlateLookupServiceApi
+    {
+        [Post("/MyServiceNSW/apexremote")]
+        Task<Object> SendServiceNswRequest();
     }
 }
