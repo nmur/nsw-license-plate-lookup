56b0f8d2725b6a6eaca21cc03cdca6367c47c94c
Nicholas Murray <nickmurray91@gmail.com>
2020-02-24 01:37:00 +1100

    first successful integration test!

--- tst/int/NswLicensePlateLookupIntTests/PlateLookupControllerIntTests.cs
+++ tst/int/NswLicensePlateLookupIntTests/PlateLookupControllerIntTests.cs
@@ -9,6 +9,7 @@ using WireMock.Server;
 using WireMock.Settings;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
+using WireMock.Matchers;
 using System.Net.Http;
 using System;
 using System.IO;
@@ -66,12 +67,33 @@ namespace NswLicensePlateLookupTests
                     Port = 10321,
                 }   
             );
-            _server.Given(Request.Create().WithPath("/MyServiceNSW/apexremote").UsingPost())
+
+            _server
+                .Given(
+                    Request
+                    .Create()
+                    .WithPath("/MyServiceNSW/apexremote")
+                    .WithBody(new JsonPathMatcher("$..[?(@.method == 'createRMSTransaction')]"))
+                    .UsingPost())
                 .RespondWith(
                     Response.Create()
                     .WithStatusCode(200)
                     .WithHeader("Content-Type", "application/json;charset=UTF-8")
                     .WithBody(JsonConvert.SerializeObject(SuccessfulTransactionTokenResponse))
+                );
+
+            _server
+                .Given(
+                    Request
+                    .Create()
+                    .WithPath("/MyServiceNSW/apexremote")
+                    .WithBody(new JsonPathMatcher("$..[?(@.method == 'postVehicleListForFreeRegoCheck')]"))
+                    .UsingPost())
+                .RespondWith(
+                    Response.Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "application/json;charset=UTF-8")
+                    .WithBody(JsonConvert.SerializeObject(SuccessfulPlateDetailsResponse))
                 );
         }
 
@@ -108,6 +130,41 @@ namespace NswLicensePlateLookupTests
                         StatusCode = 2000,
                         StatusMessage = "success",
                         Token = "valid-token"
+                    }
+            }
+        };
+
+        private List<ServiceNswResponse<PlateDetailsResult>> SuccessfulPlateDetailsResponse = new List<ServiceNswResponse<PlateDetailsResult>>
+        {
+            new ServiceNswResponse<PlateDetailsResult>
+            {
+                StatusCode = 200,
+                Type = "rpc",
+                Tid = 1,
+                Ref = false,
+                Action = "RMSWrapperCtrl",
+                Method = "createRMSTransaction",
+                Result = new PlateDetailsResult 
+                    {
+                        StatusCode = 2000,
+                        StatusMessage = "success",
+                        PlateDetails = new PlateDetails
+                        {
+                            Vehicle = new Vehicle
+                                {
+                                    BodyShape = "STATION WAGON",
+                                    Manufacturer = "VLK",
+                                    ManufactureYear = 2015,
+                                    Model = "GOLF",
+                                    NswPlateNumber = "RWAGON",
+                                    PlateType = "O",
+                                    TareWeight = 1509,
+                                    Variant = "16 2.0 6SPA R WE 26KW WAGON",
+                                    VehicleColour = "GREY",
+                                    VehicleType = "PASSENGER VEHICLES",
+                                    VinNumber = "xxxxxxxxxxxxx0823"
+                                }
+                        }
                     }
             }
         };
